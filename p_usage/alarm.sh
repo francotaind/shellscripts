5f496c9c9b97b95ed53c52550d6dc000bf2c461c
francotaind <francotaind00@gmail.com>
2024-01-04 14:14:13 +0300

    #001

---
+++ p_usage/alarm.sh
@@ -0,0 +1,7 @@
+!#/bin/bash
+
+echo "hrs to event"
+read my_var
+sleep ${my_var}
+cd /home/francota/Downloads
+google-chrome Rema-Calm-Down.mp3
---
+++ p_usage/off.sh
@@ -0,0 +1,11 @@
+/*
+*This script i use to switch off my pc after a specified period of time
+*/
+
+#!/bin/bash
+echo 'How after how many h or m'
+read my_var
+echo "system will shutoff after ${my_var}"
+sleep ${my_var}
+shutdown -h now
+
