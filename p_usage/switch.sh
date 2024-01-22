ba88ec8b8bcc601481e517de6f5806e47f2a05ab
francotaind <francotaind00@gmail.com>
2024-01-22 16:24:06 +0300

    tasksubmission#003

--- p_usage/switch.sh
+++ p_usage/switch.sh
@@ -1,3 +1,12 @@
+#My manjaro laptop usually misbehaves when the power dies due to low battery.
+#This script runs as a job in the crontab and loops after every 15mins
+#When the battery goes below 8% it shutsdown the system
+
+
+
+
+
+
 #!/bin/bash
 
 # Check if acpi command is available
@@ -10,7 +19,7 @@ fi
 check_battery() {
     battery_level=$(acpi -b | grep -P -o '[0-9]+' | tr -d '\n' | tr -d '[:space:]')
 
-    if [ "$battery_level" -le 90 ]; then
+    if [ "$battery_level" -le 9 ]; then
         echo "Battery level is $battery_level%. Shutting down..."
         shutdown -h now
     fi
@@ -19,6 +28,6 @@ check_battery() {
 # Run the check_battery function in a loop every 5 minutes
 while true; do
     check_battery
-    sleep 30  # 5 minutes
+    sleep 900  # 15 minutes
 done
 
